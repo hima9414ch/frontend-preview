a1f90591c48a22e7e5057afeef106e44dcd74326
Himanshu Chauhan <Himanshu Chauhan>
2025-02-27 23:48:16 +0530

    Updated React components from JSON

---
+++ src/components/RegisterPage/RegistrationSuccessModal.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; export const RegistrationSuccessModal = () => { return (<div id="RegistrationSuccessModal_1"><h2 id="RegistrationSuccessModal_2">Success!</h2><button id="RegistrationSuccessModal_3">Close</button></div>)}
