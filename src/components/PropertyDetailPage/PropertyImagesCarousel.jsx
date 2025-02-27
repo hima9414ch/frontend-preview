d27ce20772f8802c966397c22637765dd11574fc
Himanshu Chauhan <Himanshu Chauhan>
2025-02-27 23:10:15 +0530

    Updated React components from JSON

---
+++ src/components/PropertyDetailPage/PropertyImagesCarousel.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import images from '../assets/images';
+
+const PropertyImagesCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div id="PropertyImagesCarousel_1" className="relative w-full max-w-4xl mx-auto h-[500px] group">
+      <div
+        style={{ backgroundImage: `url(${images[currentIndex]})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer hover:bg-black/50 transition-all duration-300">
+        <button id="PropertyImagesCarousel_2" onClick={prevSlide}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </button>
+      </div>
+      
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer hover:bg-black/50 transition-all duration-300">
+        <button id="PropertyImagesCarousel_3" onClick={nextSlide}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </button>
+      </div>
+
+      <div className="absolute bottom-4 left-0 right-0">
+        <div className="flex items-center justify-center gap-2">
+          {images.map((_, index) => (
+            <div
+              id={`PropertyImagesCarousel_${index + 4}`}
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`
+                transition-all w-3 h-3 bg-white rounded-full cursor-pointer
+                ${currentIndex === index ? 'p-2' : 'bg-opacity-50'}
+              `}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyImagesCarousel;
