4986fae4c90970ee591c232b6489128cd925f422
Himanshu Chauhan <Himanshu Chauhan>
2025-02-27 23:46:53 +0530

    Updated React components from JSON

--- src/components/PropertyDetailPage/Body.jsx
+++ src/components/PropertyDetailPage/Body.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 
-import PropertyImagesCarousel from "./PropertyImagesCarousel";
+import PropertyGallery from "./PropertyGallery";
 import PropertyDescription from "./PropertyDescription";
-import AmenitiesList from "./AmenitiesList";
-import LocationMap from "./LocationMap";
-import ContactForm from "./ContactForm";
-import PriceDetails from "./PriceDetails";
-import MortgageCalculator from "./MortgageCalculator";
-import PropertyReviews from "./PropertyReviews";
+import ContactAgentForm from "./ContactAgentForm";
 
 const Body = () => {
   return (
     <div>
-      <PropertyImagesCarousel />
+      <PropertyGallery />
       <PropertyDescription />
-      <AmenitiesList />
-      <LocationMap />
-      <ContactForm />
-      <PriceDetails />
-      <MortgageCalculator />
-      <PropertyReviews />
+      <ContactAgentForm />
     </div>
   );
 };
