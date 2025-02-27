7ebe402a450693d346ea3f377e397907fd822d00
Himanshu Chauhan <Himanshu Chauhan>
2025-02-27 23:19:20 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,29 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
-import Home from "./Home/Body.jsx";
-import Register from "./Register/Body.jsx";
-import Login from "./Login/Body.jsx";
-import Dashboard from "./Dashboard/Body.jsx";
-import Property_Listings from "./Property Listings/Body.jsx";
-import Property_Detail from "./Property Detail/Body.jsx";
-import Contact from "./Contact/Body.jsx";
+import HomePage from "./HomePage/Body.jsx";
+import ListingPage from "./ListingPage/Body.jsx";
+import LoginPage from "./LoginPage/Body.jsx";
+import PropertyDetailPage from "./PropertyDetailPage/Body.jsx";
+import RegisterPage from "./RegisterPage/Body.jsx";
+import UserProfilePage from "./UserProfilePage/Body.jsx";
 
 const Body = () => {
 return (
-  <Router>
-        <Header />
+    <Header />
+      <Router>
         <Routes>
-            <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/property listings" element={<Property_Listings />} />
-        <Route path="/property detail" element={<Property_Detail />} />
-        <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<HomePage />} />
+        <Route path="/listingpage" element={<ListingPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/propertydetailpage" element={<PropertyDetailPage />} />
+        <Route path="/registerpage" element={<RegisterPage />} />
+        <Route path="/userprofilepage" element={<UserProfilePage />} />
         </Routes>
-        <Footer />
       </Router>
+    <Footer />
 );
 };
 
