51a121e1aa174062b28a7b27c437b558c0bb7a90
Himanshu Chauhan <Himanshu Chauhan>
2025-03-01 03:18:41 +0530

    Updated React components from JSON

--- src/components/Dashboard/Body.jsx
+++ src/components/Dashboard/Body.jsx
@@ -1,42 +1,19 @@
 import React from "react";
-import NavigationSidebar from "./NavigationSidebar";
-import UserProfileCard from "./UserProfileCard";
-import DashboardWidgets from "./DashboardWidgets";
-import SystemStatus from "./SystemStatus";
-import PerformanceMetrics from "./PerformanceMetrics";
-import ResourceUsage from "./ResourceUsage";
-import DataTables from "./DataTables";
-import AnalyticsCharts from "./AnalyticsCharts";
-import RecentActivities from "./RecentActivities";
-import TaskManager from "./TaskManager";
-import CalendarEvents from "./CalendarEvents";
-import QuickActions from "./QuickActions";
-import MessageCenter from "./MessageCenter";
-import NotificationsPanel from "./NotificationsPanel";
-import SettingsPanel from "./SettingsPanel";
 
-const DashboardLayout = () => {
+import UserProfile from "./UserProfile";
+import PostList from "./PostList";
+import PostEditorModal from "./PostEditorModal";
+import DeleteConfirmationModal from "./DeleteConfirmationModal";
+
+const Body = () => {
   return (
-    <div className="dashboard-container">
-      <NavigationSidebar />
-      <main className="dashboard-main">
-        <UserProfileCard />
-        <DashboardWidgets />
-        <SystemStatus />
-        <PerformanceMetrics />
-        <ResourceUsage />
-        <DataTables />
-        <AnalyticsCharts />
-        <RecentActivities />
-        <TaskManager />
-        <CalendarEvents />
-        <QuickActions />
-        <MessageCenter />
-        <NotificationsPanel />
-        <SettingsPanel />
-      </main>
+    <div>
+      <UserProfile />
+      <PostList />
+      <PostEditorModal />
+      <DeleteConfirmationModal />
     </div>
   );
 };
 
-export default DashboardLayout;
+export default Body;
