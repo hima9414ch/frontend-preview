52eaea48ac8f4657a9550546b6669d6a4880d8cc
Himanshu Chauhan <Himanshu Chauhan>
2025-03-01 03:17:27 +0530

    Updated React components from JSON

--- src/components/Home/Body.jsx
+++ src/components/Home/Body.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import HeroSection from "./HeroSection";
-import AboutUsSection from "./AboutUsSection";
-import ServicesSection from "./ServicesSection";
-import FeaturedProducts from "./FeaturedProducts";
-import TestimonialsCarousel from "./TestimonialsCarousel";
-import BlogHighlights from "./BlogHighlights";
-import CallToAction from "./CallToAction";
-import NewsletterSignup from "./NewsletterSignup";
-import ContactForm from "./ContactForm";
-import SocialMediaLinks from "./SocialMediaLinks";
+
+import SearchBar from "./SearchBar";
+import CategoryFilter from "./CategoryFilter";
+import PostList from "./PostList";
+import PostSummary from "./PostSummary";
+import ReadMoreLink from "./ReadMoreLink";
+import Sidebar from "./Sidebar";
+import PopularPosts from "./PopularPosts";
+import AuthorInfo from "./AuthorInfo";
 
 const Body = () => {
   return (
     <div>
-      <HeroSection />
-      <AboutUsSection />
-      <ServicesSection />
-      <FeaturedProducts />
-      <TestimonialsCarousel />
-      <BlogHighlights />
-      <CallToAction />
-      <NewsletterSignup />
-      <ContactForm />
-      <SocialMediaLinks />
+      <SearchBar />
+      <CategoryFilter />
+      <PostList />
+      <PostSummary />
+      <ReadMoreLink />
+      <Sidebar />
+      <PopularPosts />
+      <AuthorInfo />
     </div>
   );
 };
