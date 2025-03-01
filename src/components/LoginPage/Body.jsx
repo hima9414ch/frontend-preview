dc5b2ded66bc7befac40c759f3d86e2d122b584d
Himanshu Chauhan <Himanshu Chauhan>
2025-03-01 13:19:51 +0530

    update

--- src/components/LoginPage/Body.jsx
+++ src/components/LoginPage/Body.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
 import LoginForm from "./LoginForm";
-import PasswordRecoveryLink from "./PasswordRecoveryLink";
-import RegistrationLink from "./RegistrationLink";
 
 const Body = () => {
   return (
     <div>
       <LoginForm />
-      <PasswordRecoveryLink />
-      <RegistrationLink />
     </div>
   );
 };
